d5a4ffe736b58198e85c2aea99c08602f929c067
mithu <mithu@gmail.com>
2020-08-20 13:39:12 +0600

    re-edit nav, add .product-balance section add

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 window.addEventListener("DOMContentLoaded", () => {
 
+	const addBtns = document.querySelectorAll(".add");
+
+
+	//events=====================
+	addBtns.forEach(addBtn => {
+		addBtn.addEventListener("click", addProduct);
+	});
+
+	//add product==================
+	function addProduct(e) {
+		const target = e.target;
+		const parent = target.parentElement;
+		console.log(parent);
+	}
+
+	//remove product==================
+	function removeProduct() {}
+
+	//calculated price===============
+	function calcPrice() {}
+
+	//calculated total product=============
+	function calcTotalProduct() {}
+
 });
